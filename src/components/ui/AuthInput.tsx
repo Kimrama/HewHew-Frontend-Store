dc8172d7f928c23bb68ff2c0dbabd20fa86debc8
Kimrama <kimrama0x@gmail.com>
2025-09-10 01:21:55 +0700

    refactor: simplify AuthInput component structure and styling

--- src/components/ui/AuthInput.tsx
+++ src/components/ui/AuthInput.tsx
@@ -15,22 +15,17 @@ export default function AuthInput({
     ...props
 }: AuthInputProps) {
     return (
-        <div className="input w-md mb-8">
-            <span
-                className={`icon-[${icon}] text-base-content/80 my-auto size-5 shrink-0`}
-            ></span>
-            <div className="input-floating group">
-                <input
-                    type={type}
-                    placeholder={placeholder}
-                    className="ps-4"
-                    id={id}
-                    {...props}
-                />
-                <label className="input-floating-label" htmlFor={id}>
-                    {label}
-                </label>
-            </div>
+        <div className="w-md mb-8">
+            <label className="label-text" htmlFor={id}>
+                {label}
+            </label>
+            <input
+                type={type}
+                placeholder={placeholder}
+                className="input"
+                id={id}
+                {...props}
+            />
         </div>
     );
 }
