b3c7a8c306ed938ddba00c9e517bba6b19555e66
Kimrama <kimrama0x@gmail.com>
2025-09-22 21:40:33 +0700

    feat: refactor Tag component to improve tag selection logic and add delete functionality

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -1,24 +1,56 @@
-import { type Tag } from "../../api/menu";
-import { useState } from "react";
+import { type Tag as TagItem } from "../../api/menu";
+import { useState, useEffect } from "react";
+
 interface TagProps {
-    tag: Tag;
-    selectTag: (tagId: string) => void;
+    tag: TagItem;
+    selectTag: (tag: TagItem) => void;
+    erasable?: boolean;
+    onDelete?: (tagId: string) => void;
+    tagSelected?: TagItem[];
 }
 
-export default function Tag({ tag, selectTag }: TagProps) {
+export default function Tag({
+    tag,
+    selectTag,
+    erasable = false,
+    onDelete,
+    tagSelected = [],
+}: TagProps) {
     const [isSelected, setIsSelected] = useState(false);
+
+    useEffect(() => {
+        if (
+            tagSelected.find((t) => t.TagID.toString() === tag.TagID.toString())
+        ) {
+            setIsSelected(true);
+        }
+    }, [tagSelected, tag.TagID]);
+
     function handleClick() {
-        selectTag(tag.TagID.toString());
+        if (!isSelected && tagSelected.length >= 2) return;
+        selectTag(tag);
         setIsSelected((prev) => !prev);
     }
+    function handleDelete() {
+        onDelete && onDelete(tag.TagID.toString());
+    }
     return (
-        <span
-            className={`badge rounded-lg user-select-none select-none cursor-pointer transition-colors duration-300 ${
-                isSelected ? " bg-black text-white" : "bg-white text-black"
-            }`}
-            onClick={handleClick}
-        >
-            {tag.Topic}
-        </span>
+        <div className="relative">
+            {erasable && (
+                <div className="absolute -top-2 -right-2 z-10 border-2 border-white rounded-full bg-red-500 text-white w-5 h-5 flex justify-center items-center cursor-pointer">
+                    <button onClick={handleDelete} className="text-xs">
+                        &times;
+                    </button>
+                </div>
+            )}
+            <span
+                className={`badge rounded-lg user-select-none select-none cursor-pointer transition-colors duration-300 ${
+                    isSelected ? " bg-black text-white" : "bg-white text-black"
+                }`}
+                onClick={handleClick}
+            >
+                {tag.Topic}
+            </span>
+        </div>
     );
 }
