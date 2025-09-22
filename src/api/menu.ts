52566ccc8d44add60d47d35fb26e3839e6e6a2a1
Kimrama <kimrama0x@gmail.com>
2025-09-22 21:40:27 +0700

    feat: implement createMenu function with multipart form data handling; add deleteTag function for tag management

--- src/api/menu.ts
+++ src/api/menu.ts
@@ -19,7 +19,41 @@ export interface MenuItem {
     Tag2ID: string | null;
 }
 
+export interface CreateMenuRequest {
+    name: string;
+    detail: string;
+    price: number;
+    image: File | null;
+    tag1ID: string | null;
+    tag2ID: string | null;
+}
+export interface CreateMenuResponse {
+    message: string;
+}
+
 const BASE_URL = import.meta.env.VITE_API_URL;
+
+export async function createMenu(
+    payload: CreateMenuRequest
+): Promise<CreateMenuResponse> {
+    const formData = new FormData();
+    formData.append("name", payload.name);
+    formData.append("detail", payload.detail);
+    formData.append("price", payload.price.toString());
+    if (payload.image) {
+        formData.append("image", payload.image);
+    }
+    formData.append("tag1_id", payload.tag1ID ? payload.tag1ID : "");
+    formData.append("tag2_id", payload.tag2ID ? payload.tag2ID : "");
+
+    const { data } = await axios.post(`${BASE_URL}/v1/menu`, formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    });
+    return data;
+}
 
 export async function getMenuList(): Promise<MenuItem[]> {
     const { data } = await axios.get<MenuItem[]>(`${BASE_URL}/v1/menu`, {
@@ -43,5 +77,28 @@ export async function getTagList(): Promise<TagListResponse> {
     );
     return data;
 }
+export async function deleteTag(tagId: string): Promise<void> {
+    const { data } = await axios.delete(`${BASE_URL}/v1/shop/tags/${tagId}`, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+    });
+    return data;
+}
 
-export async function createTag(): Promise;
+export async function createTag(topic: string): Promise<any> {
+    const { data } = await axios.post<Tag>(
+        `${BASE_URL}/v1/shop/tags`,
+        {
+            topic: topic,
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        }
+    );
+    return data;
+}
